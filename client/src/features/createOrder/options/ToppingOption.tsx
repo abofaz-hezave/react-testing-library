c90e1bbe032ddff1caca3e55f9df6bfbc76510fb
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2023-01-09 20:49:49 +0330

    updated ToppingOption to pass test in client-createOrder

--- client/src/features/createOrder/options/ToppingOption.tsx
+++ client/src/features/createOrder/options/ToppingOption.tsx
@@ -1,7 +1,16 @@
+import Col from 'react-bootstrap/Col';
 import { OptionItem } from '../types';
 
 function ToppingOption({ name, imagePath }: OptionItem) {
-  return <div />;
+  return (
+    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
+      <img
+        width="75%"
+        src={`http://localhost:3030${imagePath}`}
+        alt={`${name} topping`}
+      />
+    </Col>
+  );
 }
 
 export default ToppingOption;
