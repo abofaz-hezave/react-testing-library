7f0a788cd48ceda795803a77dcaf5bd0be577efd
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2023-01-10 23:31:23 +0330

    updated CreateOrder.test to add Provider in client-createOrder

--- client/src/features/createOrder/CreateOrder.test.tsx
+++ client/src/features/createOrder/CreateOrder.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { rest } from 'msw';
+import { Provider } from 'react-redux';
+import { setupStore } from '../../app/store';
 import CreateOrder from './index';
 import { server } from '../../mocks/server';
 
@@ -12,7 +14,11 @@ test('shows alert when scoops and toppings routes return error', async () => {
       res(ctx.status(500))
     )
   );
-  render(<CreateOrder />);
+  render(
+    <Provider store={setupStore()}>
+      <CreateOrder />
+    </Provider>
+  );
 
   const alerts = await screen.findAllByRole('alert', {
     name: 'An unexpected error happend. Please try again later.',
