6e4e8bea56780043cd5310e8ef34cf354210cab4
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2023-01-25 14:17:03 +0330

    updated ScoopOption to show input and pass the tests in client-createOrder

--- client/src/features/createOrder/options/ScoopOption.tsx
+++ client/src/features/createOrder/options/ScoopOption.tsx
@@ -1,7 +1,9 @@
-import Col from 'react-bootstrap/Col';
-import { OptionItem } from '../types';
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import { ItemComponentProps } from '../types';
 
-function ScoopOption({ name, imagePath }: OptionItem) {
+function ScoopOption({ name, imagePath, updateOptionsCount }: ItemComponentProps) {
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img
@@ -9,6 +11,22 @@ function ScoopOption({ name, imagePath }: OptionItem) {
         src={`http://localhost:3030${imagePath}`}
         alt={`${name} scoop`}
       />
+      <Form.Group
+        controlId={`${name}-count`}
+        as={Row}
+        style={{ marginTop: "10px" }}
+      >
+        <Form.Label column xs="6" style={{ textAlign: "right" }}>
+          {name}
+        </Form.Label>
+        <Col xs="5" style={{ textAlign: "left" }}>
+          <Form.Control
+            type="number"
+            defaultValue={0}
+            onChange={(event) => updateOptionsCount(name, +event.target.value)}
+          />
+        </Col>
+      </Form.Group>
     </Col>
   );
 }
