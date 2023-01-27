da909236b29a122253e55a124563702c51d5f80a
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2023-01-27 15:38:51 +0330

    updated ToppingOption to show the checkbox and pass the test in client-createOrder

--- client/src/features/createOrder/options/ToppingOption.tsx
+++ client/src/features/createOrder/options/ToppingOption.tsx
@@ -1,14 +1,20 @@
 import Col from 'react-bootstrap/Col';
-import { OptionItem } from '../types';
+import { ItemComponentProps } from '../types';
+import Form from "react-bootstrap/Form";
 
-function ToppingOption({ name, imagePath }: OptionItem) {
+function ToppingOption({ name, imagePath, updateOptionsCount }: ItemComponentProps) {
   return (
-    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
+    <Col xs={6} sm={4} md={3} lg={2} style={{ textAlign: "center" }}>
       <img
-        width="75%"
-        src={`http://localhost:3030${imagePath}`}
+        style={{ width: "75%" }}
+        src={`http://localhost:3030/${imagePath}`}
         alt={`${name} topping`}
       />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check type="checkbox"
+          onChange={(event) => updateOptionsCount(name, event.target.checked ? 1 : 0)}
+          label={name} />
+      </Form.Group>
     </Col>
   );
 }
