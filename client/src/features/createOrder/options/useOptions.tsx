de1ab67eeeab7b7ce284ee71c01d20a9ba560e04
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2023-01-28 23:01:49 +0330

    created commonUtils and added formatCurrency to it in client-utils

--- client/src/features/createOrder/options/useOptions.tsx
+++ client/src/features/createOrder/options/useOptions.tsx
@@ -2,19 +2,11 @@ import { useMemo } from 'react';
 import { useAppSelector } from "../../../app/hooks";
 import { OptionsProps } from '../types';
 import { ItemPrices } from "../../../constants";
+import { formatCurrency } from "../../../utils/commonUtils";
 
 function Options({ optionType }: OptionsProps) {
   const selectedOptions = useAppSelector((state) => state.options[optionType])
   const title = optionType[0].toUpperCase() + optionType.slice(1).toLocaleLowerCase()
-
-
-  const formatCurrency = (amount: number) => {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: "USD",
-      minimumFractionDigits: 2
-    }).format(amount)
-  }
 
   const calculation = useMemo(() => {
     const countsArray = Object.values(selectedOptions);
